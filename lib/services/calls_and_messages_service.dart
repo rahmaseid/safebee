e001f6ee2ced6370f8e3e7c23263a08a24b49515
kanegee1 <95996309+kanegee1@users.noreply.github.com>
2024-01-27 17:03:19 -0600

    added contact page functionality

---
+++ lib/services/calls_and_messages_service.dart
@@ -0,0 +1,8 @@
+
+      //Services to be able to use the native phone call and SMS features
+import 'package:url_launcher/url_launcher.dart';
+
+class CallsAndMessagesService {
+  void call(String number) => launch("tel:$number");
+  void sendSms(String number) => launch("sms:$number");
+}
